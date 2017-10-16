8ea2119c08a18aa5f07d243e1dcc3cf4d22e829f
Daria Yankevich <daria.yankevich@kelnik.ru>
2017-10-16 22:24:52 +0300

    Replace serenity logo in popup

--- index.php
+++ index.php
@@ -175,7 +175,7 @@ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = 
 	<title>Russian Blockchain Week 2017 Конференция по блокчейн</title>
 	<meta name="keywords" content="russian blockchain week, blockchain week, конференция блокчейн, blockchain конференция, конференция биткоин, блокчейн семинар, биткоин семинар, обучение блокчейн, обучение  blockchain, консультации блокчейн" />
 	<meta name="description" content="Russian blockсhain Week (Российская неделя блокчейн и криптовалют) — это главное событие года в своей индустрии, которое объединяет в себе 4 формата: конференция, выставка и шоу блокчейн-проектов, Blockchain.EXPO и Blockchain.NIGHT" />
-	<link href="css/style.css?v=7" type="text/css" rel="stylesheet"/>
+	<link href="css/style.css?v=8" type="text/css" rel="stylesheet"/>
 	<link href="css/fonts.css" type="text/css" rel="stylesheet"/>
 	<link href="css/jquery.formstyler.css" type="text/css" rel="stylesheet"/>
 	<link href="css/jquery.formstyler.theme.css" type="text/css" rel="stylesheet"/>
@@ -5339,7 +5339,7 @@ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = 
     <div class="b-sponsor__popup">
         <div class="b-sponsor__item-top">
             <div class="b-sponsor__title">SERENITY FINANCIAL</div>
-            <a href="Serenity-financial.io" target="_blank" class="b-sponsor__img serenity"></a>
+            <a href="Serenity-financial.io" target="_blank" class="b-sponsor__img serenity_transparent"></a>
         </div>
          <div class="b-video__wrapper">
              <div class="b-video b-video-sp-3">
@@ -5383,7 +5383,7 @@ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = 
     <div class="b-sponsor__popup">
         <div class="b-sponsor__item-top">
             <div class="b-sponsor__title">SERENITY FINANCIAL</div>
-            <a href="Serenity-financial.io" target="_blank" class="b-sponsor__img serenity"></a>
+            <a href="Serenity-financial.io" target="_blank" class="b-sponsor__img serenity_transparent"></a>
         </div>
          <div class="b-video__wrapper">
             <div id="youtube-sponsor-video-se"></div>
